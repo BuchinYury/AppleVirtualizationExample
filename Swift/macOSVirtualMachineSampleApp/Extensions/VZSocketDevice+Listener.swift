e202abe9d7458a9273acb95018198be325513fe9
y.d.buchin <y.d.buchin@tinkoff.ru>
2024-07-13 16:12:37 +0300

    drop objc + some changes

---
+++ Swift/macOSVirtualMachineSampleApp/Extensions/VZSocketDevice+Listener.swift
@@ -0,0 +1,9 @@
+//
+//  VZSocketDevice+Listener.swift
+//  macOSVirtualMachineSampleApp-Swift
+//
+//  Created by Yuriy Buchin on 12.07.2024.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import Foundation
